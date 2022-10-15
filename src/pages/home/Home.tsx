c6ae62091b4dec89fef3f35e1e84cf8b012ff936
Seyed Amir Mehrizi <s.a.mehrizi1989@gmail.com>
2022-10-15 20:19:44 +0330

    add filter box to the home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,151 +2,36 @@ import React from 'react'
 import styles from './home.module.css'
 function Home() {
   return (
-    <main role="main" className="flex-shrink-0">
-      <div className="container">
-        <h1 className="mt-5">Sticky footer with fixed navbar</h1>
-        <p className="lead">Pin a footer to the bottom of the viewport in desktop browsers with this custom HTML and
-          CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the
-          <code>main &gt; .container</code>.
-        </p>
-        <p>Back to <a href="/docs/4.6/examples/sticky-footer/">the default sticky footer</a> minus the navbar.</p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
+    <main className='container-fluid d-flex h-100 w-100 py-4'>
+      <div className={styles.filterContainer}>
+        <div className="card">
+          <div className="card-body">
+            <div className="form-group">
+              <label htmlFor="sel1">Color</label>
+              <select className="form-control" id="sel1">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <label htmlFor="sel1">Manufacturer</label>
+              <select className="form-control" id="sel1">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+              </select>
+            </div>
+            <div className='d-flex justify-content-end'>
+            <button className='button'>Filter</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.carListing}>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod ducimus doloribus qui nostrum!
-          Dolorem vero laboriosam praesentium omnis ratione.
-        </p>
       </div>
 
     </main>
