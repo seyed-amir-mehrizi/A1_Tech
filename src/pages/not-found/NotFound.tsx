3bdf8477c7efd0db8f53cc5b081cd4c74b470332
Seyed Amir Mehrizi <s.a.mehrizi1989@gmail.com>
2022-10-17 10:54:45 +0330

    add not found page to the app

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import styles from './notFound.module.css'
+import logo from '../../assets/images/logo.png';
+import { Link } from 'react-router-dom';
+
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className='d-flex flex-column align-items-center justify-content-center mt-5'>
+      <img src={logo} alt='auto1 logo' width={250} />
+      <h3 className='my-3'>
+        404 - Not Found
+      </h3>
+      <p>
+        Sorry, the page you are looking for does not exist.
+      </p>
+      <p>
+        you can always go back to the <Link to='/' className="text-orange">homepage</Link>
+      </p>
+    </div>
   )
 }
 
